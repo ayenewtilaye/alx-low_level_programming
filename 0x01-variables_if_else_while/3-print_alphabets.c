1b46e7c6215c413bd64d5277376c4d2ddf592978
ayenewtilaye <ayenewtilaye065@gmail.com>
2022-04-08 08:14:32 -0700

    lwup

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,14 +2,16 @@
 
 /**
  * main - entry point
- * Description: Print the lowercase alphabet
- * Return: 0
- */
+  * Description: Print lower and upper alphabet on the same line
+  * Return: 0
+  */
 int main(void)
 {
 int c;
 for (c = 0; c < 26; ++c)
 putchar('a' + c);
+for (c = 0; c < 26; ++c)
+putchar('A' + c);
 putchar('\n');
 return (0);
 }
