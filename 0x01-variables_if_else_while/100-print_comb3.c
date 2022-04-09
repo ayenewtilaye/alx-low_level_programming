db4f99e0581bf6ab82453ac57efd3d4c7e769a99
ayenewtilaye <ayenewtilaye065@gmail.com>
2022-04-09 01:45:06 -0700

    smallest

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,13 +13,14 @@ while (i <= '9')
 while (j <= '9')
 {
 if (!(i > j || i == j))
-{
+										{
 putchar(i);
 putchar(j);
 if (i == '8' && j == '9')
 {
 putchar('\n');
-}										else
+}
+else
 {
 putchar(',');
 putchar(' ');
